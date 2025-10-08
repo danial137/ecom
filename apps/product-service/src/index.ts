7b19aecc41a0b98c826bbaae3ce9003ac3d6182d
danial137 <danial79fakhrabadi@gmail.com>
2025-10-08 18:54:42 +0330

    add api endpoint and hono services to projects

--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express,{Request,Response} from "express"
 import cors from 'cors'
 const app = express();
 
@@ -6,6 +6,10 @@ app.use(cors({
     origin: ['http://localhost:3002', 'http://localhost:3003'],
     credentials: true,
 }))
+
+app.get("/", (req:Request, res:Response) => {
+  res.json("product endpoint work!")
+})
 
 app.listen(800, () => {
     console.log("product services is running on port 800");
