599b292228ec79e30064afa689c4b25e0b9ded06
danial137 <danial79fakhrabadi@gmail.com>
2025-10-13 22:49:19 +0330

    everything up to date

--- apps/product-service/src/controllers/product.controller.ts
+++ apps/product-service/src/controllers/product.controller.ts
@@ -23,8 +23,23 @@ export const createProduct = async (req: Request, res: Response) => {
   res.status(201).json(product);
 };
 
-export const updateProduct = async (req: Request, res: Response) => {};
-export const deletepProduct = async (req: Request, res: Response) => {};
+export const updateProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data: Prisma.ProductUpdateInput = req.body;
+
+  const updateProduct = await prisma.product.update({
+    where: { id: Number(id) },
+    data,
+  });
+  return res.status(200).json(updateProduct);
+};
+export const deleteProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const deletedProduct = await prisma.product.delete({
+    where: { id: Number(id) },
+  });
+  return res.status(200).json(updateProduct);
+};
 export const getProducts = async (req: Request, res: Response) => {
   const { sort, category, search, limit } = req.query;
   const orderBy = (() => {
@@ -50,22 +65,27 @@ export const getProducts = async (req: Request, res: Response) => {
         break;
     }
   })();
-const product = await prisma.product.findMany({
-  where: {
-    categorySlug: category ? (category as string) : undefined,
-    name: search
-      ? {
-          contains: search as string,
-          mode: "insensitive",
-        }
-      : undefined,
-  },
-  orderBy,
-  take: limit ? Number(limit) : undefined,
-});
+  const product = await prisma.product.findMany({
+    where: {
+      categorySlug: category ? (category as string) : undefined,
+      name: search
+        ? {
+            contains: search as string,
+            mode: "insensitive",
+          }
+        : undefined,
+    },
+    orderBy,
+    take: limit ? Number(limit) : undefined,
+  });
   res.status(200).json(product);
 };
 export const getProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
 
-    
+  const product = await prisma.product.findUnique({
+    where: { id: Number(id) },
+  });
+
+  return res.status(200).json(product);
 };
