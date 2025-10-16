e2535749e28ca23d4d5a94857c45a2014e1fcf27
danial137 <danial79fakhrabadi@gmail.com>
2025-10-16 20:21:20 +0330

    add stripe on project

--- apps/payment-service/src/index.ts
+++ apps/payment-service/src/index.ts
@@ -2,6 +2,7 @@ import { serve } from "@hono/node-server";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { Hono } from "hono";
 import { shouldBeUser } from "./middleware/authMiddleware.js";
+import stripe from "./utils/stripe.js";
 
 const app = new Hono();
 app.use("*", clerkMiddleware());
@@ -12,12 +13,29 @@ app.get("/health", (c) => {
     timeStamp: Date.now(),
   });
 });
-app.get("/test",shouldBeUser, (c) => {
-return c.json({
-  message: "You are logged in!",userId:c.get("userId")
-  
+
+app.post("/create-stripe-product", async (c) => {
+  const res = await stripe.products.create({
+    id: "123",
+    name: "Test Product",
+    default_price_data: {
+      currency: "usd",
+      unit_amount: 10 * 100,
+    },
+  });
+
+  return c.json(res);
 });
+
+app.get("/stripe-product-price", async (c) => {
+  const res = await stripe.prices.list({
+    product:"123"
+  })
+
+  return c.json(res);
 });
+
+
 
 const start = async () => {
   try {
